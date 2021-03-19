79eef8ac9f44de808c8b8a8a39a1246305bef874
Keith <Keith@inputlogic.ca>
2021-03-19 15:32:03 -0700

    add parallax effect and pictures

--- components/spotlight/index.tsx
+++ components/spotlight/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Parallax } from 'react-scroll-parallax';
 import styles from './spotlight.module.scss'
 import utilStyles from 'styles/utils.module.scss'
 import { cn } from 'lib/cn'
@@ -7,16 +8,15 @@ export default function Spotlight({edgeStyle, id, nextId, imgSrc, children}:
   {edgeStyle: string, id: string, nextId: string, imgSrc: string, children: React.ReactNode}) {
   return (
     <section id={id} className={cn(styles.spotlight, edgeStyle)}>
-    {/* <h1>PLEASE</h1> */}
-    <span className={cn(styles.image, utilStyles.image, utilStyles.fit, styles.main)}>
-    {/* <span className={styles.content}> */}
-      <Image
-        src={imgSrc}
-        alt=""
-        width={1600}
-        height={1050}
-      />
-    </span>
+    <Parallax className={cn(styles.image, utilStyles.image, utilStyles.fit, styles.main)} y={[-75, 75]} tagOuter="span">
+      <div className={utilStyles.overlay}>
+        <Image
+          src={imgSrc}
+          alt=""
+          layout='fill'
+        />
+      </div>
+    </Parallax>
     <div className={styles.content}>
       {children}
     </div>
